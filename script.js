fe982cbe81d022c2a8c83df3d53acb5721d58157
Pranita <pranitapanchal13@gmail.com>
2024-04-17 20:56:16 +0530

    Length of String

---
+++ script.js
@@ -0,0 +1,10 @@
+function displayLength() {
+    
+    var inputString = document.getElementById("inputString").value;
+    
+   
+    var length = inputString.length;
+    
+
+    document.getElementById("lengthOutput").value = length;
+}
